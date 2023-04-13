32a7b595ed182203d5acd7ea65bf243f79a5e2f3
Ingerid Dale <ingerid.dale@nb.no>
2023-04-13 12:47:49 +0200

    data: validation report + output data
    Signed-off-by: Ingerid Dale <ingerid.dale@nb.no>

--- convert_ndt2ud.sh
+++ convert_ndt2ud.sh
@@ -4,6 +4,7 @@ NDT_FILE=data/ndt_nb_${PARTITION}_udmorph.conllu
 CONVERTED=data/grew_output_${PARTITION}.conllu
 TEMPFILE=tmp.conllu
 
+echo "Convert treebank"
 
 grew transform \
     -i  $NDT_FILE \
@@ -12,6 +13,7 @@ grew transform \
     -strat main \
     -safe_commands
 
+echo "Fix punctuation"
 cat $CONVERTED | udapy -s ud.FixPunct > $TEMPFILE
 
 grew transform \
@@ -20,14 +22,16 @@ grew transform \
     -grs rules/post_udapy_fixes.grs \
     -safe_commands
 
-
+echo "Validate treebank"
 python ../tools/validate.py --max-err 0 --lang no $CONVERTED 2>&1 | tee validation-report_ndt2ud.txt
 
 # endre verdien til -e (errortype) for å hente ut linjene til en spesifikk feiltype fra valideringsrapporten
 python extract_errorlines.py -f validation-report_ndt2ud.txt -e rel-pos-advmod
 
+echo "Remove comment lines"
 python parse_conllu.py -rc -f $CONVERTED -o $TEMPFILE
 
+echo "Visualize converted treebank"
 java -jar dist-20141005/lib/MaltEval.jar -s $TEMPFILE -v 1
 
 
