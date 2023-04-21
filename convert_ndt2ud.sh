d6c038094e0ce9ecfc5e54255d5dc0155f0831df
Ingerid Dale <ingerid.dale@nb.no>
2023-04-21 15:06:42 +0200

    test apposition conversion only
    Signed-off-by: Ingerid Dale <ingerid.dale@nb.no>

--- convert_ndt2ud.sh
+++ convert_ndt2ud.sh
@@ -1,15 +1,14 @@
-TEMPFILE=tmp.conllu
-MALTGOLD=malt_input.conllu
 
 # Treebank file names
 #PARTITION=train
 #NDT_FILE=data/ndt_nb_${PARTITION}_udmorph.conllu
 #CONVERTED=data/grew_output_${PARTITION}.conllu
+#TEMPFILE=tmp.conllu
 
 # File names for testing
 NDT_FILE=data/sentences/testsents.conllu
 CONVERTED=OUTPUT.conllu
-
+TEMPFILE=deleteme.conllu
 
 
 # START CONVERSION
@@ -18,30 +17,30 @@ echo "--- Convert treebank ---"
 grew transform \
     -i  $NDT_FILE \
     -o  $CONVERTED \
-    -grs  rules/mainstrategy.grs \
-    -strat main \
+    -grs  rules/NDT_to_UD.grs \
     -safe_commands
 
-echo "--- Fix punctuation ---"
-cat $CONVERTED | udapy -s ud.FixPunct > $TEMPFILE
+#echo "--- Fix punctuation ---"
+#cat $CONVERTED | udapy -s ud.FixPunct > $TEMPFILE
 
-grew transform \
-    -i $TEMPFILE \
-    -o $CONVERTED \
-    -grs rules/post_udapy_fixes.grs \
-    -safe_commands
+#grew transform \
+#    -i $TEMPFILE \
+#    -o $CONVERTED \
+#    -grs rules/post_udapy_fixes.grs \
+#    -safe_commands
 
 # EVALUATION
-echo "--- Validate treebank ---"
-python ../tools/validate.py --max-err 0 --lang no $CONVERTED 2>&1 | tee validation-report_ndt2ud.txt
+#echo "--- Validate treebank ---"
+#python ../tools/validate.py --max-err 0 --lang no $CONVERTED 2>&1 | tee validation-report_ndt2ud.txt
 
-python extract_errorlines.py \
-    -f validation-report_ndt2ud.txt #\
+#python extract_errorlines.py \
+#    -f validation-report_ndt2ud.txt #\
     # -e right-to-left-appos  # hent ut linjene for en spesifikk feilmeldingstype (-e errortype) fra valideringsrapporten
 
 
 # VISUALISATION
 echo "--- Remove comment lines ---"
+MALTGOLD=malt_input.conllu
 python parse_conllu.py -rc -f $CONVERTED -o $TEMPFILE
 python parse_conllu.py -rc -f $NDT_FILE -o $MALTGOLD
 
