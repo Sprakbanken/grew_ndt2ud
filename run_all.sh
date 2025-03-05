c603a6bd88cbb5941a477b1592be8b6f0f746099
Ingerid Dale <ingerid.dale@nb.no>
2025-03-05 15:36:36 +0100

    add script to convert all parts of the dataset in one go

---
+++ run_all.sh
@@ -0,0 +1,6 @@
+./convert_ndt2ud.sh -l nb -p dev
+./convert_ndt2ud.sh -l nb -p test
+./convert_ndt2ud.sh -l nb -p train
+./convert_ndt2ud.sh -l nn -p dev
+./convert_ndt2ud.sh -l nn -p test
+./convert_ndt2ud.sh -l nn -p train
